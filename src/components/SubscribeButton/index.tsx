23f5533b2581432ab3cc06fd6f36101a134f9a7c
Luiz Felipe Castro <felipe1693@gmail.com>
2021-09-12 01:34:26 +0100

    first commit

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.scss'
+
+interface SubscriptionButtonProps {
+    priceId: string
+}
+
+export function SubscribeButton({ priceId }: SubscriptionButtonProps) {
+    return (
+        <button type="button" className={styles.subscribeButton}>
+            Subscribe Now 
+        </button>
+    )
+}
